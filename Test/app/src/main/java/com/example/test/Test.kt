767d14f52e737f21b1c76f9aa4cb7e865059f08a
minji-0420 <pongiii17@gmail.com>
2023-08-07 09:44:02 +0900

    Introduce Myself 추가 과제 구현(selector 활용) 이모지 배경 제거, padding 수정 이모지 사이즈 수정

--- Test/app/src/main/java/com/example/test/Test.kt
+++ Test/app/src/main/java/com/example/test/Test.kt
@@ -3,9 +3,3 @@ package com.example.test
 
 //lines.map { List(abs(it[1] - it[0])) { r -> List(2) { c -> (it[0] + r) + c } } }
 
-class Solution {
-    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
-        var answer = arrayOf<IntArray>()
-        return answer
-    }
-}
