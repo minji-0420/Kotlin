79a78abaffcf6b1296a0c0fc3e0f99c02dfcb5f6
minji-0420 <pongiii17@gmail.com>
2023-09-14 17:00:37 +0900

    [PACK] : 🎁 뷰페이저 선택된 탭 색깔 변경

--- SearchMedia/app/src/main/java/com/example/searchmedia/VPAdapter.kt
+++ SearchMedia/app/src/main/java/com/example/searchmedia/VPAdapter.kt
@@ -1,4 +1,18 @@
 package com.example.searchmedia
 
-class VPAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class VPAdapter (fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> SearchFragment()
+            1 -> BookmarkFragment()
+            else -> throw IllegalArgumentException("잘못된 위치: $position")
+        }
+    }
 }
