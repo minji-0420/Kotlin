03f26e81bffe5ad2702fc9d2f86e96b01dff9abf
minji-0420 <pongiii17@gmail.com>
2023-09-17 23:38:53 +0900

    [FEAT] : ⚙️ - imageDeliverViewModel 구현ImageSearchViewHolder 내 imageDeliverViewModel 연결

--- SearchMedia/app/src/main/java/com/example/searchmedia/ui/view/BookmarkFragment.kt
+++ SearchMedia/app/src/main/java/com/example/searchmedia/ui/view/BookmarkFragment.kt
@@ -6,10 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.searchmedia.databinding.FragmentBookmarkBinding
+import com.example.searchmedia.databinding.FragmentSearchBinding
 
 class BookmarkFragment : Fragment() {
 
-    private val binding: FragmentBookmarkBinding by lazy { FragmentBookmarkBinding.inflate(layoutInflater)}
+    private var _binding: FragmentBookmarkBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
